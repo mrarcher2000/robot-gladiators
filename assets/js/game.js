a93b12de3ee92444080756314292825a22768a5c
unknown <archernich@gmail.com>
2021-02-11 17:18:41 -0700

    fix mix case bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,33 @@
+var fightOrSkip = function() {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    if (promptFight === "skip") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+            
+            return true;
+        } 
+    }
+
+    return false;
+}
+
 var fight = function(enemy) {
     while(enemy.health > 0 && playerInfo.health > 0) {
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        if (fightOrSkip()) {
+            break;
+        };
 
-        if (promptFight === "skip" || promptFight === "SKIP") {
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            if (confirmSkip) {
-                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money)
-                break;
-            } else {
-                fight();
-            }
-            window.alert(playerInfo.name + " has chosen to skip the fight!");
-        }
-
-        else if (promptFight === "fight" || promptFight === "FIGHT") {
             //Subtract value of 'playerInfo.attack' from value of 'enemy.health' and use result to update value of 'enemy.health' variable
             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
             
@@ -52,9 +63,6 @@ var fight = function(enemy) {
             } else {
                 window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
             }
-        } else {
-            window.alert("You need to choose a valid option. Try again!");
-        }
     }
 };
 
